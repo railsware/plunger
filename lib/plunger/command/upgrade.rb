32f3af48117df4c41fd5e2bcad896ab441e85474
Andriy Yanko <andriy.yanko@gmail.com>
2011-10-19 10:52:33 +0300

    19781017: Implemented upgrade command

--- lib/plunger/command/upgrade.rb
+++ lib/plunger/command/upgrade.rb
@@ -1,3 +1,5 @@
+require 'rubygems/dependency_installer'
+
 module Plunger
   module Command
     class Upgrade
@@ -11,12 +13,27 @@ module Plunger
         end
 
         def autorun?
-          false
+          true
         end
       end
 
       def run
-        p "UPGRADE"
+        Command.ui.say("Checking plunger update ...")
+
+        req = Gem::Requirement.new(">#{Plunger::VERSION}")
+        dep = Gem::Dependency.new('plunger', req)
+        tuples = Gem::SpecFetcher.fetcher.fetch(dep)
+
+        if tuples.empty?
+          Command.ui.say("Plunger is up to date")
+        else
+          spec, source = tuples.first
+          Command.ui.say("Upgrading #{Plunger::VERSION} to #{spec.version}")
+          installer = Gem::DependencyInstaller.new
+          installer.install(spec.name, spec.version.to_s)
+          exit
+        end
+
         true
       end
     end
